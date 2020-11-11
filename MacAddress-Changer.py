97095edd28ba44f464b4d8172943ab905487c13e
HarshitDawar55 <harshitr.dawar55@gmail.com>
2020-11-12 01:53:27 +0530

    Started adding CLI!

--- MacAddress-Changer.py
+++ MacAddress-Changer.py
@@ -1,4 +1,7 @@
 
+import optparse
+
+
 
 interface = input("Enter the Interface for which the Mac Address has to be changed: ")
 macAddress = input("Enter the New Mac Address to be assigned to the Interface: ")
