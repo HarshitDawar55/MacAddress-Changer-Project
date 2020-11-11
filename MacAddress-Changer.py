2bb66f1f92a91c707611cc169d718e73e35ab937
HarshitDawar55 <harshitr.dawar55@gmail.com>
2020-11-12 01:58:41 +0530

    Added Command Line Arguments

--- MacAddress-Changer.py
+++ MacAddress-Changer.py
@@ -1,7 +1,18 @@
-
 import optparse
 
+# Initializing the Parser
+parser = optparse.OptionParser()
+parser.add_option("--interface",
+                  "-i",
+                  dest = "interface",
+                  help = "Enter the Interface for which the Mac Address has to be changed")
+
+parser.add_option("--mac_Address",
+                  "-m",
+                  dest = "macAddress",
+                  help = "Enter the New Mac Address to be assigned to the Interface")
+
+values, variables = parser.parse_args()
 
 
-interface = input("Enter the Interface for which the Mac Address has to be changed: ")
-macAddress = input("Enter the New Mac Address to be assigned to the Interface: ")
+
